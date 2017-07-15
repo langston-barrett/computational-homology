6567a76d8c8a9e0d206639ebd7988336505e525e
Langston Barrett <langston.barrett@gmail.com>
2017-07-15 01:16:33 +0000

    add license, readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Computational Homology
+
+Caution: This code and documentation are not ready for prime-time :)
+
+This project holds some code related
+to research I'm doing with [Prof. Safia Chetteh](http://people.reed.edu/~safia/)
+on the homology of the configuration spaces of graphs.
+
--- default.nix
+++ default.nix
@@ -2,16 +2,11 @@
 { pkgs ? import ./nix/pinned-pkgs.nix { } }:
 
 with pkgs; python3Packages.buildPythonPackage rec {
-  pname = "";
+  pname = "computational-homology";
   version = "0.1.0";
   name = "${pname}-${version}";
 
-  src = ./.;
-
-  # The checkInputs attribute is cleared or deleted or something by
-  # buildPythonPackage, so we need a dummy attribute to reference from shell.nix.
-  check_inputs = [ ];
-  checkInputs = check_inputs;
+  src = if lib.inNixShell then null else ./.;
 
   propagatedBuildInputs = with python3Packages; [
     docker
@@ -19,7 +14,7 @@ with pkgs; python3Packages.buildPythonPackage rec {
 
   meta = with lib; {
     homepage = https://github.com/siddharthist/computational-homology;
-    description = "";
+    description = "See README";
     maintainers = with maintainers; [ siddharthist ];
     platforms = platforms.linux;
   };
