56760a5b3bf1875e06ab40d06eadd8ffd4c0256e
Langston Barrett <langston.barrett@gmail.com>
2017-08-04 23:37:59 +0000

    nix: use python27 for sage compatibility

--- default.nix
+++ default.nix
@@ -1,16 +1,12 @@
 # -*- mode: nix -*-
 { pkgs ? import ./nix/pinned-pkgs.nix { } }:
 
-with pkgs; python3Packages.buildPythonPackage rec {
+with pkgs; python27Packages.buildPythonPackage rec {
   pname = "computational-homology";
   version = "0.1.0";
   name = "${pname}-${version}";
 
   src = if lib.inNixShell then null else ./.;
-
-  propagatedBuildInputs = with python3Packages; [
-    docker
-  ];
 
   meta = with lib; {
     homepage = https://github.com/siddharthist/computational-homology;
--- shell.nix
+++ shell.nix
@@ -6,15 +6,19 @@
 # http://nixos.org/nix/manual/#sec-nix-shell
 
 let self = pkgs.callPackage ./default.nix { };
-in with pkgs; with pkgs.python3Packages; buildPythonPackage {
+in with pkgs; with pkgs.python27Packages; buildPythonPackage {
   name = self.name;
 
   buildInputs = self.propagatedBuildInputs ++ [
+    git
+
     # Documentation
     sphinx
 
     # Development
     # autoflake # "SPC m r i" to remove unused imports in Spacemacs
+    flake8
+    pycodestyle
     setuptools
     yapf
   ];
