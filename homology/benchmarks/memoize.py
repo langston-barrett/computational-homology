ada1cc493a61c35fb98362526679e8d869f6d814
Langston Barrett <langston.barrett@gmail.com>
2017-08-04 23:37:11 +0000

    rework benchmarks with pytest.parametrize

---
+++ homology/benchmarks/memoize.py
@@ -0,0 +1,16 @@
+""" Shamelessly stolen from the Python Decorator Library
+
+https://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+"""
+import functools
+
+def memoize(obj):
+    cache = obj.cache = {}
+
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = obj(*args, **kwargs)
+        return cache[key]
+    return memoizer
--- homology/benchmarks/test_elementary_collapses.py
+++ homology/benchmarks/test_elementary_collapses.py
@@ -1,40 +1,39 @@
+# -*- coding: utf-8 -*-
 import pytest
 from homology.abrams_y import the_complex
-from homology.cubical_complex import CubicalComplex
 from homology.elementary_collapses import collapse_all
 
-C2Y = the_complex(3)
-C3Y = the_complex(3)
+from homology.benchmarks.memoize import memoize
+from sage.interfaces.chomp import have_chomp
+assert have_chomp() is True
 
-# C4Y = the_complex(4, maximality_check=False)
-# C4YC = collapse_all(the_complex(4, maximality_check=False))
-
-
-def test_have_chomp():
-    from sage.interfaces.chomp import have_chomp
-    assert have_chomp() == True
+NS = [2, 3]
 
 
-def mark(grp, f, *args, **kwargs):
-    @pytest.mark.benchmark(group=grp)
-    def g(benchmark):
-        benchmark(f, *args, **kwargs)
-    return g
+@memoize
+def the_complex_(n, collapsed):
+    comp = the_complex(n)
+    return collapse_all(comp) if collapsed else comp
 
 
-test_C2Y = mark("construction", the_complex, 2)
-test_C3Y = mark("construction", the_complex, 3)
-test_C2Y_no_chk = mark(
-    "construction", the_complex, 2, maximality_check=False)
-test_C3Y_no_chk = mark(
-    "construction", the_complex, 3, maximality_check=False)
-test_C4Y_no_chk = mark(
-    "construction", the_complex, 4, maximality_check=False)
+@pytest.mark.benchmark(group="construction")
+@pytest.mark.parametrize("n", NS)
+def test_construction(benchmark, n):
+    benchmark(the_complex, n)
 
-test_bench_C2Y = mark("homology", C2Y.homology)
-test_bench_C3Y = mark("homology", C3Y.homology)
-# test_bench_C4Y = mark(C4Y.homology)
 
-test_bench_C2Y_chomp = mark("homology", C2Y.homology, algorithm="chomp")
-test_bench_C3Y_chomp = mark("homology", C3Y.homology, algorithm="chomp")
-# test_bench_C4Y_chomp = mark(C4Y.homology, algorithm="chomp")
+@pytest.mark.benchmark(group="collapse")
+@pytest.mark.parametrize("n", NS)
+def test_collapse_n(benchmark, n):
+    benchmark(collapse_all, the_complex_(n, False))
+
+
+@pytest.mark.benchmark(group="homology")
+# @pytest.mark.parametrize("dim", [None, (0, 1)])
+@pytest.mark.parametrize("algorithm", ["auto", "no_chomp"])
+@pytest.mark.parametrize("n", NS)
+def test_homology(benchmark, n, algorithm):  #, dim):
+    if not (n == 4 and algorithm == "no_chomp"):
+        benchmark(the_complex(n, False).homology, dim=dim, algorithm=algorithm)
+    else:
+        benchmark(lambda x: x is None, None)
