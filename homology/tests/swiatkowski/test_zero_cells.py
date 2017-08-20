103cd3599a18fc5af3669394dfaa8833385f2579
Langston Barrett <langston.barrett@gmail.com>
2017-08-20 19:10:59 +0000

    swiatkowski/zero_cells: add more complete tests

--- homology/tests/swiatkowski/test_zero_cells.py
+++ homology/tests/swiatkowski/test_zero_cells.py
@@ -5,6 +5,7 @@ from homology.swiatkowski.zero_cells import *
 from homology.tests.graph_hypothesis import random_graph
 from hypothesis import strategies
 from sage.graphs.graph import Graph
+from math import factorial
 import sage.all
 import hypothesis
 import itertools
@@ -34,18 +35,38 @@ def test_zero_cells_star(n):
     """
     assert sum(1 for _ in zero_cells(1, sage.all.graphs.StarGraph(n))) == n + 1
 
+@hypothesis.given(strategies.integers(min_value=0, max_value=6))
+def test_zero_cells_star(n):
+    """\
+    There are P(n, n) = n! configurations of a n points on the interval.
+    """
+    assert sum(1 for _ in zero_cells(n, Graph([(0, 1)]))) == factorial(n)
 
-# TODO: uncomment me
-# @hypothesis.given(strategies.integers(min_value=0, max_value=2),
-#                   random_graph(max_vertices=5, max_edges=5))
-# @hypothesis.example(1, Graph([[0, 1]]))
-# @hypothesis.settings(max_examples=100)
-# def test__zero_cells_with_duplicates(n, graph):
-#     for configuration in _zero_cells_with_duplicates(n, graph, logger=logger):
-#         # Configurations are just labelings, they should be isomorphic
-#         assert configuration.is_isomorphic(graph)
 
-#         # There should be a combined total of n points on edges and vertices
-#         on_vs = filter(lambda t: t[1] is not None, configuration.vertices())
-#         on_es = list(itertools.chain(*configuration.edge_labels()))
-#         assert len(on_vs) + len(on_es) == n
+@hypothesis.given(strategies.integers(min_value=0, max_value=2),
+                  random_graph(max_vertices=5, max_edges=5))
+@hypothesis.example(1, Graph([[0, 1]]))
+@hypothesis.settings(max_examples=100)
+def test_zero_cells(n, graph):
+    """\
+    In each configuration of n points on a random graph, all n points should be
+    present on a vertex or an edge, and no point should be repeated. Further,
+    configurations are just assignments of labels on G, they should be
+    isomorphic to G.
+    """
+    for configuration in zero_cells(n, graph, logger=logger):
+
+        # Gather a list of points on vertices
+        on_vertices = []
+        for vertex_label in configuration.vertices():
+            try:
+                on_vertices.append(vertex_label[1])
+            except TypeError:
+                pass
+
+        points = list(itertools.chain(on_vertices, *configuration.edge_labels()))
+        points_set = frozenset(points)
+
+        assert configuration.is_isomorphic(graph)  # configurations are isomorphic
+        assert points_set == frozenset(xrange(n))  # all points are present
+        assert len(points) == len(points_set)  # no points are repeated
