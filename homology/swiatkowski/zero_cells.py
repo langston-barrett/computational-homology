7e67a283883c54195350cd59d519730960816933
Langston Barrett <langston.barrett@gmail.com>
2017-08-25 03:41:33 +0000

    zero_cells: rename 'point' to 'particle'

--- homology/swiatkowski/zero_cells.py
+++ homology/swiatkowski/zero_cells.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """\
-Generate the 0-cells, or configurations, of n points on a graph in the
+Generate the 0-cells, or configurations, of n particles on a graph in the
 Swiatkowski model.
 """
 import sage.all
@@ -48,15 +48,15 @@ def invert_permutation(tup):
 def _permutation_to_vertex_relabel(permutation, G):
     """\
     Get a dictionary of new labels for vertices based on a permutation of the
-    points on an Abrams-subdivision of G.
+    particles on an Abrams-subdivision of G.
 
     Runtime: O(V+n)
     """
     relabeling = dict()
-    for point, position in enumerate(permutation):
-        # If the point is on a vertex, then the key will exist
+    for particle, position in enumerate(permutation):
+        # If the particle is on a vertex, then the key will exist
         try:
-            relabeling[position] = (position, point)
+            relabeling[position] = (position, particle)
         except KeyError:
             pass
 
@@ -65,7 +65,7 @@ def _permutation_to_vertex_relabel(permutation, G):
 
 def _permutation_to_edge_relabel(permutation, G):
     """\
-    Get new labels for each edge based on a permutation of the points on an
+    Get new labels for each edge based on a permutation of the particles on an
     Abrams-subdivision of G.
 
     Runtime: O(En)
@@ -76,7 +76,7 @@ def _permutation_to_edge_relabel(permutation, G):
         # The interval on the edge is inclusive, so we have to add 1 to the end
         for label in xrange(endpoints[0], endpoints[1] + 1):
             # If a label is a key in the inverted permutation, there is a
-            # point on it. Thus, we should append that point to the label.
+            # particle on it. Thus, we should append that particle to the label.
             try:
                 # TODO: replace edge labels with their endpoints in subdivision
                 new_label.append(inverted[label])
@@ -86,16 +86,16 @@ def _permutation_to_edge_relabel(permutation, G):
 
 
 def zero_cells(n, G, logger=default_logger):
-    """ A generator of configurations of n points on G
+    """ A generator of configurations of n particles on G
 
     They are encoded as copies of G with vertices and edges labeled by which
-    points are on them. The (branched) vertices can only have one point
+    particles are on them. The (branched) vertices can only have one particle
     occupying them, so they are either labeled as:
 
-     * ``(n, None)``: No point is on vertex n
-     * ``(n, m)``: The point with label m is on vertex n
+     * ``(n, None)``: No particle is on vertex n
+     * ``(n, m)``: The particle with label m is on vertex n
 
-    The edges can have n points on them. The edge labels are (ordered) lists of
+    The edges can have n particles on them. The edge labels are (ordered) lists of
     combined length <= n.
 
     Examples:
@@ -106,46 +106,48 @@ def zero_cells(n, G, logger=default_logger):
         >>> list(zero_cells(1, Graph({})))
         []
 
-     * Up to homotopy, there is one configuration of a point on the interval:
+     * Up to homotopy, there is one configuration of a particle on the interval:
 
         >>> get_edges = lambda cnfgs: map(lambda G: G.edges(), cnfgs)
         >>> I = Graph([(0, 1)])
         >>> list(get_edges(zero_cells(1, I)))
         [[(0, 1, (0,))]]
 
-     * There are two configurations of two points on the interval:
+     * There are two configurations of two particles on the interval:
 
         >>> sorted(list(get_edges(zero_cells(2, I))))
         [[(0, 1, (0, 1))], [(0, 1, (1, 0))]]
 
-     * One point on the Y can be on any leg or on the central vertex:
+     * One particle on the Y can be on any leg or on the central vertex:
 
         >>> from sage.all import *
         >>> sum(1 for _ in zero_cells(1, graphs.StarGraph(3)))
         4
 
     Implementation: the graph is subdivided Abrams-style, so that each edge has
-    n additional labels on it. We loop through the permutations of n points on
+    n additional labels on it. We loop through the permutations of n particles on
     these n+(n*E) labels.
 
      * If the label represents a vertex, that vertex is renamed as a tuple
-       (original_label, point_on_this_vertex).
-     * If the label represents a vertex, we append that point to the list of
-       points on that edge. This is done with order kept in mind, see
+       (original_label, particle_on_this_vertex).
+     * If the label represents a vertex, we append that particle to the list of
+       particles on that edge. This is done with order kept in mind, see
        _permutation_to_edge_relabel.
 
     """
+    assert not G.is_directed()
+
     logger.debug("n: {}".format(n))
     G = subdivide(n, G)
     logger.debug("Graph: {}".format(G))
 
     # A list of every available label on this graph. Construction is O(n*E).
-    branched_vertices = (n for n, deg in enumerate(G.degree()) if deg >= 3)
+    branched = (v for v, deg in zip(G.vertices(), G.degree()) if deg >= 3)
     labels = itertools.chain(branched_vertices, *G.edge_labels())
     labels = list(labels)
     logger.debug("Labels: {}".format(labels))
 
-    # Loop through all n-tuples of point positions
+    # Loop through all n-tuples of particle positions
     # Gather necessary relabelings of H
     relabeling_set = set()
     for perm in itertools.permutations(labels, n):
